456f83053736f2395683e0c20e8bede3f2def5f1
Kamikid <kidfu@live.com>
2018-07-08 21:23:42 +1200

    Coding MapData - placeMarkers&fetchDataFromWiki

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import Map from './components/Map';
+import Map from './components/MapData';
 import './App.css';
 
 class App extends Component {
@@ -7,52 +7,52 @@ class App extends Component {
         let neighborhood = {
             name: 'Wellington',
             location: {
-                'lat': -41.2911023,
-                'lng': 174.774506
+                'lat': -41.2881279,
+                'lng': 174.7729331
             }
         };
 
         let attractions = [
             {
-                name:'Wellington Cable Car',
-                location:{
-                    'lat':-41.2898654,
-                    'lng':174.7759936
+                name: 'Wellington Cable Car',
+                location: {
+                    'lat': -41.2889937,
+                    'lng': 174.775704
                 }
             },
             {
-                name:'Te Papa',
-                location:{
-                    'lat':-41.2898654,
-                    'lng':174.7759936
+                name: 'Te Papa',
+                location: {
+                    'lat': -41.2904563,
+                    'lng': 174.7799007
                 }
             },
             {
-                name:'Caffe Astoria',
-                location:{
-                    'lat':-41.2889778,
-                    'lng':174.7740472
+                name: 'Caffe Astoria',
+                location: {
+                    'lat': -41.282442,
+                    'lng': 174.7742523
                 }
             },
             {
-                name:'Zealandia',
-                location:{
-                    'lat':-41.2911023,
-                    'lng':174.774506
+                name: 'Zealandia',
+                location: {
+                    'lat': -41.2901575,
+                    'lng': 174.7513049
                 }
             },
             {
-                name:'Mount Victoria Lookout',
-                location:{
-                    'lat':-41.2911023,
-                    'lng':174.774506
+                name: 'Mount Victoria Lookout',
+                location: {
+                    'lat': -41.2960571,
+                    'lng': 174.7921217
                 }
             },
             {
-                name:'Beehive',
-                location:{
-                    'lat':-41.2911023,
-                    'lng':174.774506
+                name: 'Beehive',
+                location: {
+                    'lat': -41.2784228,
+                    'lng': 174.7745033
                 }
             }
         ];
@@ -60,7 +60,7 @@ class App extends Component {
         return (
             <div role='application'>
                 <Map
-                    APIKey={}
+                    APIKey={'AIzaSyASvs13OzRrqsVTuy6jeNuzoU2CaB0-K4g'}
                     neighborhood={neighborhood}
                     attractions={attractions}
                 />
--- src/components/MapData.js
+++ src/components/MapData.js
@@ -7,46 +7,102 @@ import PropTypes from 'prop-types';
         src=
             "https://maps.googleapis.com/maps/api/js?key=MYAPIKEY&v=3&callback=initMap">
 </script>
-*/}
-function asyncLoadMap(url){
+*/
+}
+
+function asyncLoadMap(url) {
     let script = document.createElement('script');
     script.type = 'text/javascript';
-    script.async=true;
+    script.async = true;
     script.src = url;
 
     let target = document.getElementsByTagName('script');
-    target[0].parentNode.insertBefore(script,target);
+    target[0].parentNode.insertBefore(script, target);
+}
+
+function fetchDataFromWiki(marker){
+    let url = 'https://en.wikipedia.org/w/api.php?action=query&origin=*&prop=extracts&exintro&titles=' + marker.name + '&format=json&utf8'
+    fetch(url,{
+        method: 'POST',
+        headers: new Headers({
+            'Api-User-Agent': 'Example/1.0'
+        })
+    }).then((response)=>{
+        if(response.ok){
+            return response.json();
+        }
+        throw new Error('RESPONSE NOT OK: '+response.statusText);
+    }).then((data)=>{
+        return data.query.pages[Object.keys(pages)[0]];
+    });
 }
 
 class MapData extends Component {
     static propTypes = {
         APIKey: PropTypes.string.isRequired,
-        neighborhood:PropTypes.object.isRequired,
-        attractions:PropTypes.array.isRequired
+        neighborhood: PropTypes.object.isRequired,
+        attractions: PropTypes.array.isRequired
     };
 
     state = {
         APIKey: '',
         map: {},
-        info: {},
+        neighborhood: {},
         attractions: [],
-        markersOnMap: [],
-        icon: {},
-        highlight: {}
+        markers: []
     };
 
     componentDidMount() {
-        window.setup=this.setup;
-        asyncLoadMap('https://maps.googleapis.com/maps/api/js?key=AIzaSyASvs13OzRrqsVTuy6jeNuzoU2CaB0-K4g&callback=setup')
+        window.setup = this.setup;
+        asyncLoadMap('https://maps.googleapis.com/maps/api/js?key=' + this.props.APIKey + '&callback=setup')
     }
 
     setup = () => {
-
+        let map = this.initMap(this.props.neighborhood);
+        this.setState(
+            {
+                APIKey: this.props.APIKey,
+                map: map,
+                neighborhood: this.props.neighborhood,
+                attractions: this.props.attractions
+            }, () => {
+                this.placeMarkers(map, this.state.attractions, infoWindow);
+            }
+        )
     };
 
-    mapInit = (neighborhood) => {
+    initMap = (neighborhood) => {
+        return new windows.google.maps.Map(document.getElementById('map'), {
+            center: neighborhood.location,
+            zoom: 12
+        });
+    };
 
+    placeMarkers = (map, attractions) =>{
+        let markers = [];
+        attractions.forEach((attract)=>{
+            /*
+             * https://developers.google.com/maps/documentation/javascript/markers#add
+             */
+            let marker = new google.maps.Marker({
+                position:attract.location,
+                map:map,
+                title:attract.name
+            });
+
+            //https://developers.google.com/maps/documentation/javascript/infowindows
+            let infoWindow = new google.maps.InfoWindow({
+                content: fetchDataFromWiki(marker)
+            });
+
+            marker.addListener('click',function(){
+                infoWindow.open(map,marker);
+            });
+
+            markers.push(marker);
+        });
+        this.setState({markers:markers});
     }
-};
+}
 
 export default MapData
